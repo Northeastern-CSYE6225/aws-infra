3e8110dcc22b73d1be48a3d0ebd995f344f99534
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-12 23:43:43 -0500

    create public and private route tables

--- .example.tfvars
+++ .example.tfvars
@@ -1,2 +1,4 @@
-region         = "us-east-1"
-vpc_cidr_block = "10.0.0.0/16"
+region                         = "us-east-1"
+vpc_cidr_block                 = "10.0.0.0/16"
+public_route_table_cidr_block  = "0.0.0.0/0"
+private_route_table_cidr_block = "10.1.0.0/16"
--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
   type        = string
 }
+
+variable "public_route_table_cidr_block" {
+  description = "Public route table CIDR block"
+  default     = "0.0.0.0/0"
+  type        = string
+}
+
+variable "private_route_table_cidr_block" {
+  description = "Private route table CIDR block"
+  default     = "10.1.0.0/16"
+  type        = string
+}

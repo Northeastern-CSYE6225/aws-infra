32c5fa657d47c604c33bc450db143323cceaf6ab
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-15 22:04:45 -0500

    final changes

--- .example.tfvars
+++ .example.tfvars
@@ -1,6 +1,8 @@
 region                         = ""
+vpc_name                       = ""
 vpc_cidr_block                 = ""
 public_route_table_cidr_block  = ""
 private_route_table_cidr_block = ""
 public_subnets                 = [{ cidr : "", az : "" }]
 private_subnets                = [{ cidr : "", az : "" }]
+az_state                       = ""
--- variables.tf
+++ variables.tf
@@ -4,6 +4,11 @@ variable "region" {
   type        = string
 }
 
+variable "vpc_name" {
+  description = "VPC Name"
+  type        = string
+}
+
 variable "vpc_cidr_block" {
   description = "VPC CIDR block"
   default     = "10.0.0.0/16"
@@ -24,26 +29,20 @@ variable "private_route_table_cidr_block" {
 
 variable "public_subnets" {
   description = "Public subnet CIDR blocks and availability zones"
-  type        = list(object({ cidr : string, az : string }))
+  type        = list(string)
 }
 
 variable "private_subnets" {
   description = "Private subnet CIDR blocks and availability zones"
-  type        = list(object({ cidr : string, az : string }))
+  type        = list(string)
 }
 
-# variable "vpcs" {
-#   description = "VPC details"
-#   type = list(object({
-#     vpc_cidr_block                 = string
-#     private_route_table_cidr_block = string
-#     public_subnet_cidr_blocks      = list(string)
-#     private_subnet_cidr_blocks     = list(string)
-#   }))
-#   default = [{
-#     vpc_cidr_block                 = "10.0.0.0/16"
-#     private_route_table_cidr_block = "10.1.0.0/16"
-#     public_subnet_cidr_blocks      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#     private_subnet_cidr_blocks     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-#   }]
-# }
+variable "az_state" {
+  description = "Availability zone state"
+  type        = string
+}
+
+variable "profile" {
+  description = "AWS profile"
+  type        = string
+}

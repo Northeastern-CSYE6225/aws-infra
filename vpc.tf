85e488d326e2721f9ca450896d7db53e916e572b
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-02-22 23:38:56 -0500

    fix private route table

--- vpc.tf
+++ vpc.tf
@@ -35,11 +35,6 @@ resource "aws_route_table" "public-route-table" {
 resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.assignment3.id
 
-  route {
-    cidr_block = var.private_route_table_cidr_block
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
   tags = {
     Name = "private"
   }

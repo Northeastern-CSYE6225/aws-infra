f49449f314e3141ef9db14490f0edabe9ab85d25
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-03-16 13:18:09 -0400

    add s3 bucket public access block

--- s3.tf
+++ s3.tf
@@ -39,3 +39,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access" {
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}

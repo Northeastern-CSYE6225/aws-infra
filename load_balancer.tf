6572a328fd2f17290079a761d717a9965b33442f
Karan Wadhwa <wadhwa.k@northeastern.edu>
2023-04-11 20:51:04 -0400

    install ssl certificates

--- load_balancer.tf
+++ load_balancer.tf
@@ -72,13 +72,41 @@ resource "aws_lb_target_group" "alb_tg" {
   }
 }
 
-resource "aws_lb_listener" "lb_listener" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = data.aws_acm_certificate.ssl.arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_tg.arn
   }
+}
+
+data "aws_acm_certificate" "ssl" {
+  domain   = var.domain_name
+  statuses = ["ISSUED"]
+}
+
+resource "aws_lb_listener_certificate" "lb_certificate" {
+  listener_arn    = aws_lb_listener.https.arn
+  certificate_arn = data.aws_acm_certificate.ssl.arn
 }
